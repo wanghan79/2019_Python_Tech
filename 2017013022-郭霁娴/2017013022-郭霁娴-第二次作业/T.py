d41957782e25493bfb40d73449c71ff7864287c8
guojixian <1125132729@qq.com>
2019-12-26 10:54:21 +0800

    2017013022-郭霁娴

---
+++ 2017013022-郭霁娴/2017013022-郭霁娴-第二次作业/T.py
@@ -0,0 +1,12 @@
+import typing
+
+class txtReader:
+    def parser(self,file):
+        with open(file) as f:
+           # row=0
+            container=[]
+            for line in f:
+                words = line.split(",")
+                container.append(words)
+        f.close
+        return container
