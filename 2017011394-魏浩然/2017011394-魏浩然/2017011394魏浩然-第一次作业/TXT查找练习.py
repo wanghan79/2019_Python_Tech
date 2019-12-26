ab2d59f1e901a2f27559723804cccb3f267afb17
魏浩然 2017011394 <54901804+weihr578@users.noreply.github.com>
2019-12-26 14:50:26 +0800

    2017011394-魏浩然

---
+++ 2017011394-魏浩然/2017011394-魏浩然/2017011394魏浩然-第一次作业/TXT查找练习.py
@@ -0,0 +1,21 @@
+import os
+
+def txtReader(file):
+    with open(file) as f:
+        row = 0
+        container = []
+        for line in f:
+            words=line.split(" ")
+            for col, item in enumerate(words):
+                element = (row,col, item)
+                container.append(element)
+            row += 1
+        for element in container:
+            for item in element:
+                if item =="haoran":
+                    print(element)
+
+
+    f.close()
+
+txtReader('TXT1.txt')
