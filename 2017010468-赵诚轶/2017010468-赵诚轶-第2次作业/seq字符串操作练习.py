46e738b6f05fa03ffb89348a25437fd8289621ba
2017010426-李展坤 <54655377+K-LZK@users.noreply.github.com>
2019-12-26 11:25:49 +0800

    asdadfafA

---
+++ 2017010468-赵诚轶/2017010468-赵诚轶-第2次作业/seq字符串操作练习.py
@@ -0,0 +1,11 @@
+seq = "nowadays we are loving!"
+sub = "nowtodays"
+shu = "we"
+print(seq.capitalize())
+print(seq.count(sub))
+print(seq.endswith(seq,1,5))
+print(seq.find(shu,0))
+print(seq.index(shu,0))
+print(seq.replace("are","we"))
+print(seq.rfind(sub))
+print(seq.split(' ',1))
